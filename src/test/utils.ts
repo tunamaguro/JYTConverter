bbed1fdf2c99895a297ffde695feaae9294c2e10
tunamaguro <tsu7magu6@gmail.com>
2022-04-22 21:55:42 +0900

    add type

--- src/test/utils.ts
+++ src/test/utils.ts
@@ -1,8 +1,8 @@
 /* eslint-disable import/export */
-import { render } from '@testing-library/react'
+import { render, RenderOptions } from '@testing-library/react'
 import React from 'react'
 
-const customRender = (ui: React.ReactElement, options = {}) =>
+const customRender = (ui: React.ReactElement, options: RenderOptions = {}) =>
   render(ui, {
     // wrap provider(s) here if needed
     wrapper: ({ children }) => children,
