0edff38c4dc30b44bffc540e7239caae790e9252
tunamaguro <tsu7magu6@gmail.com>
2022-05-05 01:26:54 +0900

    add handle selector

--- src/ConverterBase.tsx
+++ src/ConverterBase.tsx
@@ -1,26 +1,46 @@
-import { FC, ReactChild } from 'react'
+import { ChangeEventHandler, FC, ReactChild } from 'react'
 
-type ConverterBaseProps = { children: ReactChild }
+const options = ['JSON', 'YAML', 'TOML'] as const
 
-export const ConverterBase: FC<ConverterBaseProps> = ({ children }) => (
-  <div className="p-5">
-    <div className=" border-gray-400 border rounded-lg px-4 w-full">
-      <div className="border-b py-4 flex items-center flex-row">
-        <div className="font-roboto text-xl">JSON</div>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="ml-1 h-5 w-5"
-          viewBox="0 0 20 20"
-          fill="currentColor"
-        >
-          <path
-            fillRule="evenodd"
-            d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-            clipRule="evenodd"
-          />
-        </svg>
+type OptionsTuple = keyof {
+  [K in typeof options extends ReadonlyArray<infer U> ? U : never]: string
+}
+
+const isOptionTuple = (v: unknown): v is OptionsTuple =>
+  options.includes(v as any)
+
+type ConverterBaseProps = {
+  onSelectorChange?: (v: OptionsTuple) => void
+  children: ReactChild
+}
+
+export const ConverterBase: FC<ConverterBaseProps> = ({
+  onSelectorChange,
+  children,
+}) => {
+  const handleChange: ChangeEventHandler<HTMLSelectElement> = (e) => {
+    if (isOptionTuple(e.target.value)) {
+      onSelectorChange?.(e.target.value)
+    } else {
+      throw new Error(
+        `expected ${options.join(' ')},but received ${e.target.value}`,
+      )
+    }
+  }
+  return (
+    <div className="p-5">
+      <div className=" border-gray-400 border rounded-lg px-4 w-full">
+        <div className="border-b flex items-center flex-row">
+          <div className="font-roboto text-xl">
+            <select className="p-4" onChange={handleChange}>
+              {options.map((v) => (
+                <option key={v}>{v}</option>
+              ))}
+            </select>
+          </div>
+        </div>
+        <div className="min-h-[16rem] p-4">{children}</div>
       </div>
-      <div className="min-h-[16rem] p-4">{children}</div>
     </div>
-  </div>
-)
+  )
+}
