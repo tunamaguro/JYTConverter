df6420efbeabe780c94dd4d53943b3847b86b1b4
tunamaguro <tsu7magu6@gmail.com>
2022-05-06 01:28:44 +0900

    fix typo

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import App from './App'
-import { WasmInitalizer } from './wasm/initalizer'
+import { WasmInitializer } from './wasm/initializer'
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <WasmInitalizer>
+    <WasmInitializer>
       <App />
-    </WasmInitalizer>
+    </WasmInitializer>
   </React.StrictMode>,
 )
